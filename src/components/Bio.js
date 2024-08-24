e168c77d2f2722b3de38ec527bc7f73e51995ecd
Ahmed Wael <83090095+Euro-max@users.noreply.github.com>
2024-08-24 11:09:06 -0700

    Update Bio.js

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -12,7 +12,7 @@ axios.get(url).then((response)=>{SetData(response.data)
 })
       },[])
   return (
-    <section className=' text-white pt-[50px] bg-[#222222] mt-[-20px]' id='Bio'>
+    <section className=' text-white pt-[80px] bg-[#222222] mt-[-60px]' id='Bio'>
    <motion.p initial={{opacity:0,y:-20}} whileInView={{opacity:1,y:0}} transition={{duration:1}}className='text-4xl flex justify-center font-bold mb-[30px] text-white'>Bio</motion.p>
   <motion.div className='text-center' initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:0.8, delay:0.2}}>
       <motion.p initial={{opacity:0,x:-20}} whileInView={{opacity:1,x:0}} transition={{duration:0.8,delay:0.5}}className='p-3 text-3xl tracking-tighter lg:text-4xl '>Hello, I am Ahmed Wael, Graduated from STEM 6th of October High School for Boys in 2023 ranked 24th nationally,currently studying Computer Engineering at CUFE. I started learning CS at 15,and Literally, I was blown away by its potential.I will keep learning and developing scalable solutions to whatever problem I encounter Additionally, I am a Chess Fan and Esports Lover. I peaked at 2000 in Rapid and Blitz, 2100 in Bullet</motion.p>
